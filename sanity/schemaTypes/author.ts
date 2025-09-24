e9887eee711c146783bca7dca2c72d7a1ce0227c
Swastik <swastikraychoudhuri@gmail.com>
2025-09-24 08:51:48 +0530

    sanity schema added

---
+++ sanity/schemaTypes/author.ts
@@ -0,0 +1,37 @@
+import { User, UserIcon } from "lucide-react";
+import { defineField, defineType } from "sanity";
+
+export const author = defineType({
+    name: "author",
+    title: "Author",
+    type: "document",
+    icon: UserIcon,
+    fields: [
+        defineField({
+            name: "id",
+            type: "number"
+        }),
+        defineField({
+            name: "name",
+            type: "string"
+        }),
+        defineField({
+            name: "username",
+            type: "string"
+        }),
+        defineField({
+            name: "email",
+            type: "url"
+        }),
+        defineField({
+            name: "bio",
+            type: "text"
+        }),
+    ],
+
+    preview: {
+        select: {
+            title: "name"            
+        }
+    }
+});
